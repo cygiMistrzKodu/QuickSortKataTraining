f413730518e0360e9eb56aae855a978ba81be61b
Jacek programmer <jacek301@gmail.com>
2024-02-27 00:00:45 +0100

    First Degenerated test pass

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -1,10 +1,20 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static java.util.Arrays.asList;
+
 public class SortTest {
     @Test
     public void testSort() {
 
-        fail();
+        assertEquals(asList(), sort(asList()));
+
+    }
+
+    private List<Integer> sort(List<Integer> list) {
+        return new ArrayList<>();
     }
 }
