4d727e4a04365e8f39b4ef565ddf005fa9f35928
Jacek programmer <jacek301@gmail.com>
2024-02-27 00:03:22 +0100

    4 test 2 element w changes order

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -13,6 +13,7 @@ public class SortTest {
         assertEquals(asList(), sort(asList()));
         assertEquals(asList(1), sort(asList(1)));
         assertEquals(asList(1,2), sort(asList(1,2)));
+        assertEquals(asList(1,2), sort(asList(2,1)));
 
     }
 
