2fa3d9bef374be5b3ce8548be7eac3e6041091ae
Jacek programmer <jacek301@gmail.com>
2024-02-27 00:44:22 +0100

    Pass Test. Wykorzystane prawo Trachotomii.

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -33,7 +33,33 @@ public class SortTest {
             }
 
         } else {
-            return list;
+
+            int first = list.get(0);
+            int middle = list.get(1);
+            int last = list.get(2);
+
+            List<Integer> lessers = new ArrayList<>();
+            List<Integer> greaters = new ArrayList<>();
+
+            if (first < middle) {
+                lessers.add(first);
+            }
+            if (last < middle) {
+                lessers.add(last);
+            }
+            if (first > middle) {
+                greaters.add(first);
+            }
+            if (last > middle) {
+                greaters.add(last);
+            }
+
+            List<Integer> result = new ArrayList<>();
+            result.addAll(lessers);
+            result.add(middle);
+            result.addAll(greaters);
+
+            return result;
         }
 
     }
