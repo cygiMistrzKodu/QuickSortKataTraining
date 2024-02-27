d82b43bb92a7b022cb45ef0970635b851f453615
Jacek programmer <jacek301@gmail.com>
2024-02-27 23:27:00 +0100

    Next two test passes

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -20,6 +20,8 @@ public class SortTest {
         assertEquals(asList(1, 2, 3), sort(asList(1, 3, 2)));
         assertEquals(asList(1, 2, 3), sort(asList(3, 2, 1)));
         assertEquals(asList(1, 2, 3, 4), sort(asList(1, 2, 3, 4)));
+        assertEquals(asList(1, 2, 3, 4), sort(asList(2, 1, 3, 4)));
+        assertEquals(asList(1, 2, 3, 4), sort(asList(4, 3, 2, 1)));
 
     }
 
